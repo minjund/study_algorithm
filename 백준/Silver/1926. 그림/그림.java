71ebacfc0da1d979e6a8795757cf031aedd49d88
minjund <mam060922@naver.com>
2024-05-20 16:36:51 +0900

    [Silver I] Title: 그림, Time: 428 ms, Memory: 44652 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,92 +1,82 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static int[][] arr;
-    static boolean[][] visit;
-    static int[] dx;
-    static int[] dy;
-    static Queue<Pair> qu;
 
-    public static void main(String args[]) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        
-        arr = new int[n][m];
+    //세로
+    static int[] dx = {-1, 0, 1, 0}; // 상 오 하 왼
+
+    //가로
+    static int[] dy = {0, 1, 0, -1}; // 상 오 하 왼
+
+    static int[][] map;
+    static boolean[][] visit;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader sb = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(sb.readLine());
+
+        //세로
+        int n = Integer.parseInt(st.nextToken());
+        //가로
+        int m = Integer.parseInt(st.nextToken());
+
+        Queue<int[]> q = new LinkedList<>();
+
+        map = new int[n][m];
         visit = new boolean[n][m];
-        qu = new LinkedList<>();
-        
-        dx = new int[]{1, 0, -1, 0}; // 상좌하우 (x가 행 y가 열)
-        dy = new int[]{0, 1, 0, -1}; // 상좌하우
-        // 기본세팅
-        for(int i=0; i<n; i++){
-            st = new StringTokenizer(bf.readLine());
-            for(int j=0; j<m; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+
+        // 처음 맵 세팅
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(sb.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        // bfs 돌리기
-        int count = 0;
-        int area = 0;
-        int max = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(arr[i][j] == 0 || visit[i][j]){ //0이거나 방문기록이 있으면 = 다음차례로 넘어가 -> 1찾기
-                    continue;
-                }
-                count++;    
-                qu.offer(new Pair(i, j));   
-                visit[i][j] = true;     
-                area = 0;    
-                while(!qu.isEmpty()){ // 큐스택이 없을때까지
-                    Pair p = qu.poll();
+
+        int num = 0;//총 갯수,
+        int max = 0; // 가장큰 그림 값
+
+        //시작점이 2개 이상 일수 있으니 2중 포문
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(map[i][j] == 0 || visit[i][j]) continue;
+
+                num++; // 카운팅
+                visit[i][j] = true; //방문함
+                q.offer(new int[]{i,j}); //추가
+
+                int area = 0; // 영역 초기화
+
+                //q가 들어있으면 영역 카운팅
+                while (!q.isEmpty()){
                     area++;
-                    for(int k = 0; k < 4; k++){ //4카씩 체크
-                        int n_x = p.x + dx[k]; // 1
-                        int n_y = p.y + dy[k]; // 0
-                        if(n_x < 0 || n_x >= n || n_y < 0 || n_y >= m){ // 칸이 넘어갈경우는 패스
-                            continue;
-                        }
-                        if(arr[n_x][n_y] == 1 && !visit[n_x][n_y]){ // 해당 칸 방문한적이 없으면
-                            qu.offer(new Pair(n_x, n_y)); // qu에 n_x,n_y를 추가
-                            visit[n_x][n_y] = true; //방문 ture
-                        }
+                    int[] temp = q.poll();
+                    int x = temp[0];
+                    int y = temp[1];
+                    for (int k = 0; k < 4; k++) {
+                        int nx = x + dx[k];
+                        int ny = y + dy[k];
+
+                        // 비교
+                        if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                        if (visit[nx][ny] || map[nx][ny] != 1) continue;
+
+                        visit[nx][ny] = true;
+
+                        q.offer(new int[]{nx,ny});
                     }
                 }
-                if(area > max){ // 지금 지역이 max보다 크면
-                    max = area;
-                }
+                max = Math.max(max, area); // 더 큰 영역 추가
             }
-            
         }
-        System.out.println(count);
+        System.out.println(num);
         System.out.println(max);
-    }
-    
-    public static class Pair{
-        int x;
-        int y;
-        
-        public Pair(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-        
-        public int getX(){
-            return x;
-        }
-        public int getY(){
-            return y;
-        }
-        public void setX(int x){
-            this.x = x;
-        }
-        public void setY(int y){
-            this.y = y;
-        }
-        
     }
 }
